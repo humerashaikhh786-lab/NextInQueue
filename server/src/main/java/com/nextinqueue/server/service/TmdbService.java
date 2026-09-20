85b878a8e15370feb5c83ad3b281d239f3fd1f52
Humera Shaikh <humerashaikhh786@gmail.com>
2026-09-20 16:16:25 +0530

    Remove BOM from TmdbService

--- server/src/main/java/com/nextinqueue/server/service/TmdbService.java
+++ server/src/main/java/com/nextinqueue/server/service/TmdbService.java
@@ -1,4 +1,4 @@
-﻿package com.nextinqueue.server.service;
+package com.nextinqueue.server.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
